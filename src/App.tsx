4fd1023a00036151e3288231e39b88cd16e09bb9
rajput-hemant <rajput.hemant2001@gmail.com>
2023-06-08 21:25:20 +0530

    add responsiveness
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import Home from "@/components/Home";
+import Navbar from "@/components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Home/>
+    <div className="h-screen bg-gradient-to-b from-black to-gray-800">
+      <Navbar />
+
+      <main className="h-full w-full pt-20">
+        <Home />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
+import { MdKeyboardArrowRight } from "react-icons/md";
 
-import { MdKeyboardArrowRight } from 'react-icons/md';
-
-function Home() {
+const Home = () => {
   return (
-    <div className='h-screen w-full bg-gradient-to-b from-black to-gray-800  ; '>
-         <div className='max-w-screen-lg mx-auto flex flex-col item-center justify-center h-full px-4 md:flex-row '>
-            <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white ' > I'm a Full Stack Developer</h2>
-                <p className='text-gray-500 py-4 max-w-md' >
-                I am a Student.
-                Currently, I love to work on web applications using technoligies like
-                React, tailwind ,Next js and Typescript. 
-            </p>
-            <div >
-                <button className='text-white group  w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
-                    Portfolio
-                    <span className='group-hover:rotate-90 duration-300'>
-                    <MdKeyboardArrowRight></MdKeyboardArrowRight>
-                    </span>
-                   
-                </button>
-            </div>
-            </div>
-            <div className='rounded-2xl mx-auto  w-2/3 md:w-full items-center py-32 ' >
-                <img src="/luffy.png" alt="my personal profile"  />
-            </div>
-         </div>
+    <div className="item-center mx-auto flex h-full w-full max-w-screen-lg flex-col items-center justify-center px-4 md:flex-row">
+      <div className="flex w-full flex-col items-center justify-center gap-y-4">
+        <h2 className="text-center text-4xl font-bold text-white sm:text-5xl">
+          I'm a Full Stack Developer
+        </h2>
+
+        <p className="max-w-md py-4 text-center text-gray-400">
+          I am a Student. Currently, I love to work on web applications using
+          technoligies like React, tailwind ,Next js and Typescript.
+        </p>
+
+        <div>
+          <button className="group my-2 flex w-fit items-center rounded-xl bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 font-semibold text-white">
+            Portfolio
+            <span className="duration-300 group-hover:translate-x-1">
+              <MdKeyboardArrowRight className="h-5 w-5" />
+            </span>
+          </button>
+        </div>
+      </div>
+
+      <div className="flex w-full items-end justify-center md:justify-end">
+        <img
+          src="/luffy.png"
+          alt="my personal profile"
+          className="aspect-square max-w-sm object-cover"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
